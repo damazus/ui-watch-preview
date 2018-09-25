39e8b7c4d021b46fb8ea8bd0728741cb395844dc
damazus <liginmlugudor@gmail.com>
2018-09-25 11:09:57 +0000

    tab done

---
+++ dist/js/Slider.js
@@ -0,0 +1,123 @@
+class Slider{
+    constructor(element, options = {}){
+        this.$el = $(element);
+        this.defaults = {
+            speed: 500,
+            easing: 'ease'
+        };
+        this.$el.css({
+            'position': 'relative',
+            'overflow-x': 'hidden'
+        });
+        this.options = {...this.defaults, ...options};
+        this.config();
+        this.initEvents();
+    }
+
+    config(){
+        this.$list = this.$el.find('[data-slide-items]');
+        this.$items = this.$el.find('[data-slide-item]');
+        this.itemsCount = this.$items.length;
+        const transEndEventNames = {
+            'WebkitTransition' : 'webkitTransitionEnd',
+            'MozTransition' : 'transitionend',
+            'OTransition' : 'oTransitionEnd',
+            'msTransition' : 'MSTransitionEnd',
+            'transition' : 'transitionend'
+        };
+        const transformNames = {
+            'WebkitTransform' : '-webkit-transform',
+            'MozTransform' : '-moz-transform',
+            'OTransform' : '-o-transform',
+            'msTransform' : '-ms-transform',
+            'transform' : 'transform'
+        };
+        this.current = 0;
+        this.old = 0;
+        this.isAnimating = false;
+        this.$list.css({
+            display: 'flex',
+            width: 100 * this.itemsCount + '%',
+            transition: 'transform ' + this.options.speed + 'ms ' + this.options.easing
+        });
+        this.$items.css({width: 100 / this.itemsCount + '%', padding: '0 1px'})
+
+        // generate navigation
+    }
+
+    initEvents(){
+        const $dots = this.$el.find('[data-slide-dot] a');
+
+        if($dots.length){
+            const self = this;
+            $dots.on('click', function () {
+                const $me = $(this);
+                const $parent = $me.parent();
+
+                $("[data-slide-dot]").removeClass("is-active");
+                $parent.addClass("is-active");
+                self.jump($parent.index());
+            });
+        }
+    }
+
+    navigate(direction){
+        if(this.isAnimating){
+            return false
+        }
+
+        this.isAnimating = true;
+        this.old = this.current;
+
+        if(direction === 'next' && this.current < this.itemsCount - 1){
+            ++this.current;
+        }else if(direction === 'previous' && this.current > 0){
+            --this.current;
+        }
+
+        this.slide();
+    }
+
+    slide(){
+        this.toggleNavControls();
+
+        let translate = -1 * this.current * 100 / this.itemsCount
+
+        this.$list.css({
+            transform: 'translate3d('+ translate +'%, 0, 0)'
+        });
+        this.$list.on('transitionend', () => {
+            this.isAnimating = false
+            this.options.onTransitionEnd ? this.options.onTransitionEnd() :null
+        })
+    }
+
+    toggleNavControls(){
+
+    }
+
+    jump(position) {
+        if (position === this.current || this.isAnimating) return false
+
+        this.isAnimating = true;
+        this.old = this.current;
+        this.current = position;
+
+        this.slide();
+    }
+
+    destroy(){
+        // remove nav controls
+
+        this.$list.css('width', 'auto');
+        this.$list.css('transition', 'none');
+        this.$list.css('display', 'block');
+        this.$items.css({width: 'auto', padding: '0'})
+    }
+
+}
+
+const slider = new Slider("#slider", {
+    speed: 400,
+    ease: "cubic-bezier(0, 0.69, 0.19, 1)"
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,24 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let uglify = require('gulp-uglify');
-let pump = require('pump');
-let cleanCSS = require('gulp-clean-css');
 let browserSync = require('browser-sync').create();
 
-function asset(path) {
-    return "src/assets/"+path
-}
-
-function dist(path) {
-    return "src/dist/"+path
-}
-
-const _path = {
-    scss: 'src/assets/scss/**/*.scss',
-    js: {
-        src: 'src/assets/js/**/*.js',
-        dest: "src/dist/js/*.js"
-    },
-    css: {
-        src: 'src/dist/css/*.css',
-        dest: 'src/dist/css'
-    },
-    html: "src/*.html"
-};
 
 gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: "./src"
+            baseDir: "./"
         }
     });
 
-    gulp.watch(_path.scss, ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-    gulp.watch(_path.js.dest).on('change', browserSync.reload);
+    gulp.watch("./scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./dist/js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function(){
-    return gulp.src(_path.scss)
+    return gulp.src(".//scss/**/*.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(_path.css.dest))
+        .pipe(gulp.dest("./dist/css"))
         .pipe(browserSync.stream());
 });
 
